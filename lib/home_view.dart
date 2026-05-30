e4c2c1d007dbe4569fc743bfed629f038301048b
Jolut11 <jolut11@gmail.com>
2026-05-30 00:11:57 -0500

    se integra codigo flutter

---
+++ lib/home_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.blueGrey),
+      child: Center(
+        child: Container(
+          color: Colors.orange,
+          child: Column(
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(
+                      "https://cdn-icons-png.flaticon.com/512/1864/1864521.png",
+                    ),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+              const Text("Buhos y pasteles"),
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text("Pedidos"),
+                          Text("Insumos"),
+                          Text("Recetas"),
+                          Text("Finanzas"),
+                        ],
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(20),
+                            ),
+                          ),
+                          child: Text("Pedidos"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
